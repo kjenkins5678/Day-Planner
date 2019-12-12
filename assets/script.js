b721325813396913d0ed570ec327332f35cf01bd
kjenkins5678 <kajenkins9@gmail.com>
2019-12-12 14:24:24 -0500

    moved script files and improved time for loop

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,30 @@
-var dayOfWeek = moment().day();
-var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+$(document).ready(function() {
+    var dayOfWeek = moment().day();
+    var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-var month = moment().month();
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var month = moment().month();
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-var day = moment().date();
+    var plannerTimes = ['9', '10', '11', '12','1', '2', '3', '4', '5' ];
 
-var dayAndDateDiv = $("<div>");
-dayAndDateDiv.text(daysOfTheWeek[dayOfWeek]+", "+months[month] + " " + day)
+    var day = moment().date();
 
-$(".jumbotron").append(dayAndDateDiv);
+    var dayAndDateDiv = $("<div>");
+    dayAndDateDiv.text(daysOfTheWeek[dayOfWeek]+", "+months[month] + " " + day)
 
-var row = $("<div class=row>");
-$(".hours").append(row);
+    $(".jumbotron").append(dayAndDateDiv);
+
+    for (i = 0; i < plannerTimes.length; i++){
+        // console.log("planner time:" + plannerTimes[i]);
+        var row = $("<div class=row>");
+        var timeDiv = $('<div>');
+        var timeH2 = $('<h2>');
+        timeH2.text(plannerTimes[i]);
+        timeDiv.append(timeH2);
+        row.append(timeDiv);
+        $(".hours").append(row);
+    };
+
+});
+
+        
