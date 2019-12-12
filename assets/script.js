3e0c61c77682f78c6718dde269692b54498b4903
kjenkins5678 <kajenkins9@gmail.com>
2019-12-11 20:34:07 -0500

    added basic shell with some moment.js dates

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var dayOfWeek = moment().day();
+var daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+var month = moment().month();
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+var day = moment().date();
+
+var dayAndDateDiv = $("<div>");
+dayAndDateDiv.text(daysOfTheWeek[dayOfWeek]+", "+months[month] + " " + day)
+
+$(".jumbotron").append(dayAndDateDiv);
